6dc316343ca52abcf23d885868e2645786b2a209
Suman Devkota <37627609+sumandevkota@users.noreply.github.com>
2023-01-03 12:50:18 -0500

    updates on database

--- webdev/blog/views.py
+++ webdev/blog/views.py
@@ -1,36 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [ 
-    {
-        'author':'SumanD',
-        'title':'Blog post 1',
-        'content': 'First post Content',
-        'date_posted':'December 30,2022'
-    },
-    {
-        'author':'Ojashpi',
-        'title':'Blog post 2',
-        'content': 'Second post Content',
-        'date_posted':'December 25,2022'
-    },
-    {
-        'author':'Sadeep',
-        'title':'Blog post 3',
-        'content': 'Third post Content',
-        'date_posted':'December 20,2022'
-    },
-    {
-        'author':'Ashika',
-        'title':'Blog post 4',
-        'content': 'Fourth post Content',
-        'date_posted':'December 15,2022'
-    }
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 
